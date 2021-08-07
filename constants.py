68f2bc51d310375249bd6d38857fa44bf2a9d2a4
MWolfe45 <mwolfe45@gmail.com>
2021-08-07 16:26:59 -0500

    added all changes up to the development of the tool page

--- constants.py
+++ constants.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 
 # Constants for References Page
@@ -28,7 +28,7 @@ label_dict={'grocery_per1000':'Grocery Access','super_per1000':'Superstore Acces
            }
 
 
-# HEre we are creating a graphic that showcases the proportion of races that live within each obesity category. 
+# Here we are creating a graphic that showcases the proportion of races that live within each obesity category. 
 
 demo = pd.read_csv("data/demo_pop_data_2017.csv")
 merged_df = pd.read_csv("data/eda_dataframe.csv")
@@ -65,3 +65,77 @@ colors = ["#BF6380", "#E3914D","#E3E14D","#22908C","#4B0082","#0277C9","#7463BF"
 # osp.set_xlabel("Proportion of Race Demographic")
 # osp.set_ylabel("Obesity Category")
 # plt.tight_layout()
+
+
+# Making Dataframe for the selection of the right fips for county/state combinations
+county_select = pd.DataFrame()
+county_select['county_names'] = merged_df['county']
+county_select['state'] = merged_df['state']
+county_select['fips'] = merged_df['fips']
+
+county_list = merged_df['county'].to_list()
+states_list = merged_df['state'].to_list()
+states_list = list(dict.fromkeys(states_list))
+
+
+county_dicts = [{'label':'--Reset Selection--', 'value':'--Reset Selection--'}]
+states_dicts = [{'label':'--Reset Selection--', 'value':'--Reset Selection--'}]
+for county in county_list:
+    county_options = {'label':county, 'value':county}
+    county_dicts.append(county_options)
+
+for state in states_list:
+    states_options = {'label':state, 'value':state}
+    states_dicts.append(states_options)
+
+    
+    
+    
+    
+########################################################################################################################################    
+# Setting up Dataframe for modeling page
+
+# loading in obesity data from our EDA and dropping columns that have too many null values. Additonally, hawaii and alaska
+# had no inputs for obesity rates which are necessary for our model. Thus, they were dropped. 
+# furthermore, obesity proxies that went into creating healthy score access was also dropped.
+obesity_df = pd.read_csv("data/obesity_eda.csv")
+obesity_df[~obesity_df.region.str.contains('O')]
+obesity_df = obesity_df.drop(columns = ['primary_minority', 'supercenter_access_score', 'grocery_access_score', 'fullservice_access_score', 'farmersmarket_access_score', 'wic_available_per1000', 'snap_bens_per1000'])
+obesity_df['percent_other'] = obesity_df['percent_nhpi'] + obesity_df['percent_multi']
+
+# Loading in car access data
+car_access_df = pd.read_csv("data/car_access_2017.csv")
+unemployment_df = pd.read_csv("data/unemployment _2017.csv")
+unemployment_df = unemployment_df[["fips","total_unemployment_rate_over_16", "total_educated_some_college_or_associates_25_64", "pop_est_over_16"]]
+unemployment_df["some_college_rate"] = unemployment_df["total_educated_some_college_or_associates_25_64"]/unemployment_df["pop_est_over_16"]
+unemployment_df = unemployment_df[["fips","total_unemployment_rate_over_16", "some_college_rate"]]
+
+# Merged the two data sets
+obesity_df = pd.merge(obesity_df, car_access_df, how = 'left', on = 'fips')
+obesity_df = pd.merge(obesity_df, unemployment_df, how = 'left', on = 'fips')
+obesity_df = obesity_df.drop(columns = ['percent_asian_low_access_15','percent_pop_low_access_15', 'percent_low_income_low_access_15',
+       'percent_snap_low_access_15', 'percent_child_low_access_15',
+       'percent_white_low_access_15', 'percent_nhna_low_access_15','nhpi_low_access_15', 'percent_no_car_low_access_15', 'percent_senior_low_access_15',
+       'percent_black_low_access_15', 'percent_hispanic_low_access_15',
+       'percent_nhpi_low_access_15', 'percent_multiracial_low_access_15'])
+
+
+# made some numerical variables easier to read 
+obesity_df['percent_no_car'] = obesity_df['percent_no_car'] * 100
+obesity_df['pop_estimate'] = obesity_df['pop_estimate']/1000
+
+
+# Making our food dessert categorical variable. 
+conditions = [(obesity_df["fi_rate"] > 15) & (obesity_df["healthy_access_category"] != 'high'), 
+              (obesity_df["fi_rate"] <= 15)]
+values = [1, 0]
+
+obesity_df["food_dessert"] = np.select(conditions, values)
+
+# dropping any remaining null values so our model can work
+obesity_df = obesity_df.dropna()
+
+# creating dummy variables; the VIF test does not take in categorical variables
+obesity_df['region'] = obesity_df['region'].map({'N':1, 'M':2, 'S':3, 'W':4})
+obesity_df['healthy_access_category'] = obesity_df['healthy_access_category'].map({'low':1, 'medium':2, 'high':3})
+obesity_df['class_category'] = obesity_df['class_category'].map({'low_income':1, 'lower_mid_class':2, 'mid_class':3, 'highest_income': 4})
--- lib/.ipynb_checkpoints/tool-checkpoint.py
+++ lib/.ipynb_checkpoints/tool-checkpoint.py
@@ -1,11 +1,14 @@
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
-from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
+from dash.dependencies import Input, Output, State
 import sys
 sys.path.append("..")
 
 from app import app
+from model import lin_reg
+from constants import states_dicts, county_dicts, county_select, obesity_df
 
 layout = html.Div([
     dbc.Container([
@@ -21,23 +24,206 @@ layout = html.Div([
             dbc.Col(html.H5(children="To operate, please choose your specific US county from the dropdown menu. The variable fields will be populated with your county's specifc figures, which you can then alter to predict the effects of implemented changes."), className="mb-5")
         ]),
         
-        dbc.Row(
-            dbc.Col(dcc.Dropdown(),style={'width':'20%'}),
+        dbc.Row([
+            dbc.Col(dcc.Dropdown(id='county_selection', options=county_dicts, placeholder='Select your county'),style={'width':'20%'}),
+            dbc.Col(dcc.Dropdown(id='state_selection', options=states_dicts, placeholder= 'Select your state'),style={'width':'20%'})],
         ),
         dbc.Row(
-            dbc.Col(dcc.Input(style={'margin-top':'25px'}))
+            [dbc.Col(html.Label('Fips code: ')),
+             dbc.Col(html.Div(id='fips-output-container'))]
+        ),
+        
+        dbc.Row([dbc.Col(dbc.Card([
+        dbc.Row(
+            [dbc.Col(html.Label('Superstores per 1000: ')),
+            dbc.Col(html.Div(id='superstore_var'))]
         ),
         dbc.Row(
-            dbc.Col(dcc.Input(style={'margin-top':'25px'}))
+            [dbc.Col(html.Label('Grocery Stores per 1000: ')),
+            dbc.Col(html.Div(id='grocery_var'))]
         ),
         dbc.Row(
-            dbc.Col(dcc.Input(style={'margin-top':'25px'}))
+            [dbc.Col(html.Label('Convenience Stores per 1000: ')),
+            dbc.Col(html.Div(id='convenience_var'))]
         ),
         dbc.Row(
-            dbc.Col(dcc.Input(style={'margin-top':'25px'}))
+            [dbc.Col(html.Label('Specialty Stores per 1000: ')),
+            dbc.Col(html.Div(id='specialty_var'))]
         ),
         dbc.Row(
-            dbc.Col(dcc.Input(style={'margin-top':'25px'}))
+            [dbc.Col(html.Label('Fast Food Restaurants per 1000: ')),
+            dbc.Col(html.Div(id='fast_var'))]
+        ),
+        dbc.Row(
+            [dbc.Col(html.Label('Full Service Restaurants per 1000: ')),
+            dbc.Col(html.Div(id='full_var'))]
+        ),
+        dbc.Row(
+            [dbc.Col(html.Label('School Lunch Programs: ')),
+            dbc.Col(html.Div(id='lunch_var'))]
+        ),
+        dbc.Row(
+            [dbc.Col(html.Label('Percent Without Car Access: ')),
+            dbc.Col(html.Div(id='no_car_var'))]
+        ),
+        dbc.Row(
+            [dbc.Col(html.Label('Percent of Farmers Markets that Accept WIC: ')),
+            dbc.Col(html.Div(id='farm_wic_var'))]
+        ),
+        dbc.Row(
+            [dbc.Col(html.Label('Percent of Farmers Market that Accept Credit: ')),
+            dbc.Col(html.Div(id='farm_cred_var'))]
+        ),
+        dbc.Row(
+            [dbc.Col(html.Label('Percent of Farmers Markets that Sell Fresh Vegetables: ')),
+            dbc.Col(html.Div(id='farm_veg_var'))]
+        ),
+            
+        dbc.Row(
+            [dbc.Col(html.Label('Total Unemployment: ')),
+            dbc.Col(html.Div(id='unem_var'))]
         )
+        ], style={'margin-top':'25px'})),
+        dbc.Col(
+            [dbc.Row(dbc.Card([html.Label('Actual Obesity Rate: '),html.Div(id='obesity_actual')], style={'margin-top':'25px', 'width':'600px'})),
+            dbc.Row(dbc.Card())
+            ])
+                
+                ])
     ])
 ])
+
+# Callback for county selections based on state
+@app.callback(Output('county_selection','options'),
+             [Input('state_selection', 'value')])
+
+def update_options(choice):
+    
+    if choice == '--Reset Selection--':
+        if not choice:
+            raise PreventUpdate
+        return county_dicts
+    
+    else:
+        if not choice:
+            raise PreventUpdate
+        new_df = county_select.copy()
+        new_df = new_df[new_df['state']==choice]
+
+        new_county_list = new_df['county_names'].to_list()
+        new_county_dicts = [{'label':'--Reset Selection--', 'value':'--Reset Selection--'}]
+        for county in new_county_list:
+            new_county_options = {'label':county, 'value':county}
+            new_county_dicts.append(new_county_options)
+
+        return new_county_dicts
+
+# Callback for state selections based on county
+@app.callback(Output('state_selection','options'),
+             [Input('county_selection', 'value')])
+
+def update_options(choice):
+    
+    if choice== '--Reset Selection--':
+        if not choice:
+            raise PreventUpdate
+        return states_dicts
+    
+    else:
+        if not choice:
+            raise PreventUpdate
+
+        new_df = county_select.copy()
+        new_df = new_df[new_df['county_names']==choice]
+
+        new_state_list = new_df['state'].to_list()
+        new_state_dicts = [{'label':'--Reset Selection--', 'value':'--Reset Selection--'}]
+        for state in new_state_list:
+            new_state_options = {'label':state, 'value':state}
+            new_state_dicts.append(new_state_options)
+
+
+
+        return new_state_dicts
+    
+# Callback for state selections based on county
+@app.callback(Output('fips-output-container','children'),
+             Input('county_selection', 'value'),
+             Input('state_selection', 'value'),
+)
+
+def show_fips(county, state):
+    
+    
+    new_df = county_select.copy()
+    new_df = new_df[new_df['state']==state]
+    new_df = new_df[new_df['county_names']==county]
+    if new_df.empty:
+        return 'No county selected'
+    else:
+        code = new_df['fips'].iloc[0]
+    return code
+
+
+@app.callback(Output('obesity_actual', 'children'),
+              Output('superstore_var', 'children'),
+              Output('grocery_var', 'children'),
+              Output('convenience_var', 'children'),
+              Output('specialty_var', 'children'),
+              Output('fast_var', 'children'),
+              Output('full_var', 'children'),
+              Output('lunch_var', 'children'),
+              Output('no_car_var', 'children'),
+              Output('farm_wic_var', 'children'),
+              Output('farm_cred_var', 'children'),
+              Output('farm_veg_var', 'children'),
+              Output('unem_var', 'children'),
+             Input('fips-output-container', 'children'))
+
+def update_superstore(fips):
+    new_df = obesity_df.copy()
+    new_df = new_df[new_df['fips']==fips]
+    superstore_df = new_df['super_per1000']
+    if new_df.empty:
+        return '','','','','','','','','','','','',''
+
+    superstore_var = superstore_df.iloc[0]
+    grocery_df = new_df['grocery_per1000']
+    grocery_var = grocery_df.iloc[0]
+   
+    convenience_df = new_df['convenience_per1000']
+    convenience_var = convenience_df.iloc[0]
+    
+    specialty_df = new_df['specialty_per1000']
+    specialty_var = specialty_df.iloc[0]
+    
+    fast_df = new_df['fast_food_per1000']
+    fast_var = fast_df.iloc[0]
+    
+    full_df = new_df['full_service_per1000']
+    full_var = full_df.iloc[0]
+    
+    lunch_df = new_df['school_lunch_prog_17']
+    lunch_var = lunch_df.iloc[0]
+    
+    no_car_df = new_df['percent_no_car']
+    no_car_var = no_car_df.iloc[0]
+    
+    farm_wic_df = new_df['pct_fm_accept_wic']
+    farm_wic_var = farm_wic_df.iloc[0]
+    
+    farm_cred_df = new_df['pct_fm_credit']
+    farm_cred_var = farm_cred_df.iloc[0]
+   
+    farm_veg_df = new_df['pct_fm_sell_frveg']
+    farm_veg_var = farm_veg_df.iloc[0]
+    
+    unem_df = new_df['total_unemployment_rate_over_16']
+    unem_var = unem_df.iloc[0]
+    
+    ob_df = new_df['obesity_rate']
+    ob_var = ob_df.iloc[0]
+    return ob_var, superstore_var, grocery_var, convenience_var, specialty_var, fast_var, full_var, lunch_var, no_car_var, farm_wic_var, farm_cred_var, farm_veg_var, unem_var
+
+
+        
